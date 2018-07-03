d0c0376c20ba56ef7738f28d2db577d8f41105f4
georgegach <georgeggach@gmail.com>
2018-07-02 20:56:51 -0400

    new features

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","bc22e3a20423db29e645ff67c066aaad"],["index.js","4a48b2a2e3d671b0ece9c167f0aae7d6"]];
+var precacheConfig = [["index.html","bc22e3a20423db29e645ff67c066aaad"],["index.js","ed47fd6db47a2b244ac4d2630f002a2e"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
